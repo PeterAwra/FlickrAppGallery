329598c430261d50a1ab056ce01dce2e0000bce4
Peter Avramyshyn <peter.awra@.gmail.com>
2019-05-11 20:30:23 +0300

    add Retrofit,RxJava,Search + startNestScroll

--- app/src/main/java/com/stud/awra/flickrappgallery/MainActivity.java
+++ app/src/main/java/com/stud/awra/flickrappgallery/MainActivity.java
@@ -3,71 +3,67 @@ package com.stud.awra.flickrappgallery;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import com.bumptech.glide.Glide;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subjects.PublishSubject;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
   public static final String API_KEY = "26e83e1921b2d40964324325ff109858";
   public static final String FORMAT = "json";
-  public static final String METHOD = "flickr.photos.getRecent";
   private RecyclerView recyclerView;
   private SearchView searchView;
-  private PublishSubject<String> voidSingle;
   private PhotoAdapter adapter;
   private Disposable subscribeSearch;
   private Disposable subscribeRecent;
-  private CompositeDisposable compositeDisposable;
+  private NestedScrollView nestedScrollView;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     recyclerView = findViewById(R.id.rv);
     searchView = findViewById(R.id.search_view);
+    nestedScrollView = findViewById(R.id.scroll);
     recyclerView.setLayoutManager(
         new GridLayoutManager(this, 2, LinearLayoutManager.VERTICAL, false)
     );
     adapter = new PhotoAdapter();
     recyclerView.setAdapter(adapter);
-    observableSearchView(searchView);
-    voidSingle = PublishSubject.create();
-    voidSingle.onNext("0");
     getRecent();
-    compositeDisposable = new CompositeDisposable();
+    observableSearchView(searchView);
+    new Thread(() -> Glide.get(this).clearDiskCache()).start();
   }
 
   private void getRecent() {
-    new Thread(new Runnable() {
-      @Override public void run() {
-        subscribeRecent = App.getApiFlickrService()
-            .getRecent(ApiFlickrService.METHOD, ApiFlickrService.API_KEY, ApiFlickrService.FORMAT,
-                1)
-            .observeOn(Schedulers.io())
-            .map(resultPhotos -> resultPhotos.getPhotos().getPhoto())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(photos -> adapter.setData(photos), throwable -> showSnakBarError(throwable));
-      }
-    }).start();
-    //compositeDisposable.add(subscribeRecent);
+    Thread thread = new Thread(() -> subscribeRecent = App.getApiFlickrService()
+        .getRecent(ApiFlickrService.METHOD, ApiFlickrService.API_KEY, ApiFlickrService.FORMAT,
+            1)
+        .observeOn(Schedulers.io())
+        .map(resultPhotos -> resultPhotos.getPhotos().getPhoto())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(photos -> adapter.setData(photos), this::showSnakBarError,
+            () -> subscribeRecent.dispose()));
+    thread.start();
   }
 
   private void observableSearchView(SearchView searchView) {
     Observable<String> stringObservable = Observable.create(emitter -> {
       searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
         @Override public boolean onQueryTextSubmit(String query) {
-          emitter.onNext(query);
           searchView.clearFocus();
-          if (query.length() == 0) {
+          if (query.length() < 3) {
+            getRecent();
+          } else {
+            emitter.onNext(query);
           }
           return false;
         }
@@ -79,16 +75,19 @@ public class MainActivity extends AppCompatActivity {
       });
     });
     subscribeSearch = stringObservable.observeOn(Schedulers.io())
-        .debounce(300, TimeUnit.MILLISECONDS)
+        .debounce(500, TimeUnit.MILLISECONDS)
         .map(String::trim)
         .filter(s -> s.length() >= 3)
         .map(String::toLowerCase)
-        .distinctUntilChanged()
+        //.distinctUntilChanged()
         .switchMap(this::flickrGetQueryPhotos)
         .map(resultPhotos -> resultPhotos.getPhotos().getPhoto())
         .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(adapter::setData, throwable -> showSnakBarError(throwable, adapter));
-    // compositeDisposable.add(subscribeSearch);
+        .subscribe(photos -> {
+          //recyclerView.smoothScrollToPosition(0);
+          adapter.setData(photos);
+          nestedScrollView.smoothScrollTo(0, 0);
+        }, throwable -> showSnakBarError(throwable, adapter));
   }
 
   private void showSnakBarError(Throwable e, final PhotoAdapter adapter) {
@@ -110,6 +109,11 @@ public class MainActivity extends AppCompatActivity {
 
   @Override protected void onDestroy() {
     super.onDestroy();
-    compositeDisposable.dispose();
+    if (!subscribeRecent.isDisposed()) {
+      subscribeRecent.dispose();
+    }
+    if (!subscribeSearch.isDisposed()) {
+      subscribeSearch.dispose();
+    }
   }
 }
